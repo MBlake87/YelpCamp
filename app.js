f6b1d33f2e61c7e5c4481618f320993a614c13cf
MBlake87 <markblake1987@gmail.com>
2022-12-15 16:53:45 +0000

    added mongoose connectivity

--- app.js
+++ app.js
@@ -1,6 +1,18 @@
 const express = require("express");
 const app = express();
 const path = require("path");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("connected")
+});
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "Views"));
@@ -14,4 +26,4 @@ app.listen(3000, () => {
     console.log("serving on port 3000");
 })
 
-jjj
+
