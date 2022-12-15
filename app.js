afe1ef4be8bca940c78e83b0aaef7dfe26de81f7
MBlake87 <markblake1987@gmail.com>
2022-12-15 15:21:58 +0000

    Initial Commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "Views"));
+
+
+app.get("/", (req, res) => {
+    res.render("home");
+})
+
+app.listen(3000, () => {
+    console.log("serving on port 3000");
+})
+
+jjj
