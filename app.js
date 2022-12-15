24372284df1b766df3fb33147d1ed8e9225c10f2
MBlake87 <markblake1987@gmail.com>
2022-12-15 17:32:58 +0000

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
+const Campground = require("./models/campground");
 
 mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp", {
     useNewUrlParser: true,
@@ -20,6 +21,12 @@ app.set("views", path.join(__dirname, "Views"));
 
 app.get("/", (req, res) => {
     res.render("home");
+})
+
+app.get("/makecampground", async (req, res) => {
+    const camp = new Campground({ title: "My Backyard", price: "20", description: "in my garden" });
+    await camp.save();
+    res.send(camp);
 })
 
 app.listen(3000, () => {
