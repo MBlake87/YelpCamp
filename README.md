ef58658bec1bd11b1e24d3241c61916d1bdb44b3
MBlake87 <markblake1987@gmail.com>
2023-01-31 11:28:55 +0000

    commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# YelpCamp
+ YelpCamp project from Colt Steele Bootcamp
+
+Added loop that generates 50 random campsites and adds to the database
+
+Added full CRUD functionality - seperate pages for show details, edit and added delete functionality.
+
+Included bootstrap - added basic styling
+
+Set up partials for nav bar and boilerplate
+
+Added clientside validation with bootstrap
+
+Added serverside validation using Joi schemas
+
+Added the review model and integrated relationships between review and campgrounds
+
+Restructured code to use express routes
+
+Added Image Upload
---
+++ app.js
@@ -0,0 +1,101 @@
+if (process.env.NODE_ENV !== "production") {
+    require("dotenv").config()
+}
+console.log(process.env.secret)
+const express = require("express");
+const path = require("path");
+const app = express();
+const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate")
+const session = require("express-session")
+const flash = require("connect-flash");
+const ExpressError = require("./utils/ExpressError");
+const methodOverride = require("method-override");
+const passport = require("passport");
+const localPassport = require("passport-local");
+const campgroundRoutes = require("./routes/campgrounds.js");
+const reviewRoutes = require("./routes/reviews.js");
+const User = require("./models/user");
+const userRoutes = require("./routes/users")
+
+
+
+
+mongoose.set("strictQuery", false);
+mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+
+})
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error:"));
+db.once("open", () => {
+    console.log("connected")
+});
+
+const sessionConfig = {
+    secret: "thisisasecret",
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+        maxAge: 1000 * 60 * 60 * 24 * 7,
+        httpOnly: true
+    }
+
+}
+app.use(session(sessionConfig));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session())
+
+passport.use(new localPassport(User.authenticate()))
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
+app.set("view engine", "ejs");
+
+app.set("views", path.join(__dirname, "Views"));
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride("_method"))
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "public")))
+
+
+app.use((req, res, next) => {
+    console.log(req.session)
+    res.locals.currentUser = req.user;
+    res.locals.success = req.flash("success");
+    res.locals.error = req.flash("error")
+    next();
+})
+
+
+app.get("/", (req, res) => {
+    res.render("home");
+})
+
+app.use("/", userRoutes)
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/reviews", reviewRoutes)
+
+app.all("*", (req, res, next) => {
+    next(new ExpressError("Page Not Found", 404))
+})
+
+app.use((err, req, res, next) => {
+    const { statusCode = 500, } = err;
+    if (!err.message)
+        err.message = "Something Went Wrong"
+
+    res.status(statusCode).render("error", { err })
+})
+
+app.listen(3000, () => {
+    console.log("serving on port 3000");
+})
+
+
+
+
